caba10b113ba650c4e567cdf52eac1efb9aa6a9d
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-10-17 19:41:55 +0200

    catched up to #15 - Angular Services

--- noteit-ng-app/src/app/notes/notes.component.ts
+++ noteit-ng-app/src/app/notes/notes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component, OnInit} from '@angular/core';
 import {Notebook} from "./model/notebook";
+import {ApiService} from "../shared/api.service";
 
 @Component({
   selector: 'app-notes',
@@ -10,15 +10,15 @@ import {Notebook} from "./model/notebook";
 export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit() {
     this.getAllNotebooks();
   }
 
   public getAllNotebooks() {
-    let url = "http://localhost:8082/api/notebooks/all"
-    this.http.get<Notebook[]>(url).subscribe(
+    this.apiService.getAllNotebooks().subscribe(
       res => {
         this.notebooks = res;
       },
