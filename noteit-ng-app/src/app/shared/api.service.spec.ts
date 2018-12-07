c1a14f7d6795a98518e8f66d4a39d65238df93c1
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-12-07 12:30:43 +0100

    #23 Advanced Angular Testing (HttpClient Mock)

--- noteit-ng-app/src/app/shared/api.service.spec.ts
+++ noteit-ng-app/src/app/shared/api.service.spec.ts
@@ -1,12 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import {ApiService} from './api.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Notebook} from '../notes/model/notebook';
 
-// describe('ApiService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-//
-//   it('should be created', () => {
-//     const service: ApiService = TestBed.get(ApiService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+describe('ApiService', () => {
+  let httpMock: HttpTestingController;
+  let apiService: ApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
+
+    httpMock = getTestBed().get(HttpTestingController);
+    apiService = getTestBed().get(ApiService);
+  });
+
+  it('it is created', () => {
+    expect(apiService).toBeTruthy();
+  });
+
+  it('should get all notebooks from http', () => {
+    // given
+    const dummyNotebooks: Notebook[] = [
+      {id: '1', name: 'default', numberOfNotes: 0}
+    ];
+
+    // when
+    apiService.getAllNotebooks().subscribe(res => {
+      expect(res.length).toBe(1);
+      expect(res).toEqual(dummyNotebooks);
+    });
+
+    // http mock
+    const req = httpMock.expectOne(apiService.ALL_NOTEBOOKS_URL);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyNotebooks);
+
+  });
+});
